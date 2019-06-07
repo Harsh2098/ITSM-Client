078d3409bf9262104262756f9d6f483eaeeedafa
Harsh Mahajan <harshmahajan927@gmail.com>
2019-06-07 12:34:21 +0530

    Add GCP instance address

--- app/src/main/java/com/hmproductions/itsmclient/utils/Constants.java
+++ app/src/main/java/com/hmproductions/itsmclient/utils/Constants.java
@@ -2,6 +2,6 @@ package com.hmproductions.itsmclient.utils;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://192.168.43.3:3000/api/";
+    public static final String BASE_URL = "http://35.247.176.122:3000/api/";
     public static String USER_TOKEN = "";
 }
